40e978af75cbe43b353256ef7c22a9537ca80483
Antoine Richard <antoiner.info@gmail.com>
2021-12-23 21:57:56 +0100

    2021/23

---
+++ 2021/23/index.js
@@ -0,0 +1,4 @@
+module.exports = () => {
+    console.log('1.', "For this one, nothing better than a paper and pencil (See solution.jpg)")
+    console.log('2.', "Solution 2 is quite more difficult, but still doable with paper")
+}
